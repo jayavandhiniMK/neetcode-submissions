77cb8d47c4f064519f454f0437ff92687fad21a9
Jayavandhini M K <jayavandhinimk@gmail.com>
2026-08-27 23:25:01 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        double res = helper(x, Math.abs((long) n));
+        return (n >= 0) ? res : 1 / res;
+    }
+
+    private double helper(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = helper(x, n / 2);
+        return (n % 2 == 0) ? half * half : x * half * half;
+    }
+}
